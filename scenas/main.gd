7ec58fe2c37e6c70bba1b39a098d4a3a33471d8b
JamesHacker21 <139198190+JamesHacker21@users.noreply.github.com>
2024-12-15 10:06:49 -0500

    Musica
    Los detener

--- scenas/main.gd
+++ scenas/main.gd
@@ -4,13 +4,25 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	musica_juego.play()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
+func _input(event):
+	if event.is_action_pressed("ui_select"):
+		print("L")
+		musica_juego.stop()
+		music_menu.stop()
+		
+func _input1(event):
+	if event.is_action_pressed("ui_accept"):
+		print("A")
+		musica_juego.play()
+		music_menu.play()
 
-func _on_music_menu_finished() -> void:
+
+func _on_musica_juego_finished() -> void:
 	musica_juego.play()
