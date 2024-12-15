77051e80b9b8cc49daa39fa799d3c258b81cf294
JamesHacker21 <139198190+JamesHacker21@users.noreply.github.com>
2024-12-15 09:31:05 -0500

    Musica
    la musica dell juego

--- scenas/main.gd
+++ scenas/main.gd
@@ -1,11 +1,16 @@
 extends Node2D
-
+@onready var musica_juego = $Musica_Juego
+@onready var music_menu =$Music_Menu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	musica_juego.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_music_menu_finished() -> void:
+	musica_juego.play()
